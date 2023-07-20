657409a8aa4c922c1bb42f4f10162e133cf20656
shivaganeshtechis <shivaganesh@techis.io>
2023-07-20 08:41:05 +0530

    added api link

--- src/API.js
+++ src/API.js
@@ -9,7 +9,7 @@ let baseURL;
 // 	baseURL = REACT_APP_API_BASE_URL_DEV;
 // }
 
-baseURL = "http://127.0.0.1:8000/";
+baseURL = "https://z-wear-backend.shivaganesh8.repl.co";
 
 const api = axios.create({
   baseURL: baseURL,
